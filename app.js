0955431a056b1535acba02e642fedf370c96c629
Leslie Shea <leslieyshea@gmail.com>
2020-07-18 14:19:42 -0700

    created list notes function

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ yargs.command({
   command: 'list',
   describe: 'List all notes',
   handler() {
-    console.log('Listing all notes');
+    notes.listNotes();
   }
 });
 
--- notes.js
+++ notes.js
@@ -48,8 +48,17 @@ const loadNotes = () => {
   }
 }
 
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.inverse('Your Notes'));
+  notes.forEach(note => {
+    console.log(note.title);
+  });
+}
+
 module.exports = {
   getNotes: getNotes,
   addNote: addNote,
-  removeNote: removeNote
+  removeNote: removeNote,
+  listNotes: listNotes
 };
